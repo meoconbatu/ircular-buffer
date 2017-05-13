fed6bbe511dbf49aabf57d1a6350a75554380a8c
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-05-13 21:15:44 +0700

    rename variables follow Go's convention. Add IsEmpty and IsFull function

--- circular_buffer.go
+++ circular_buffer.go
@@ -5,52 +5,64 @@ import "errors"
 const testVersion = 4
 
 type Buffer struct {
-	array         []byte
-	current_index int
-	begin_index   int
-	length        int
+	array  []byte
+	tail   int
+	head   int
+	length int
 }
 
+func (buf *Buffer) IsEmpty() bool {
+	b := buf.array[buf.head]
+	if b == 0 {
+		return true
+	}
+	return false
+}
+func (buf *Buffer) IsFull() bool {
+	if buf.array[buf.tail] != 0 {
+		return true
+	}
+	return false
+}
 func NewBuffer(size int) *Buffer {
 	b := Buffer{make([]byte, size), 0, 0, size}
 	return &b
 }
 func (buf *Buffer) ReadByte() (byte, error) {
-	b := buf.array[buf.begin_index]
-	if b == 0 {
+	if buf.IsEmpty() {
 		return 0, errors.New("The buffer is empty!")
 	}
-	buf.array[buf.begin_index] = 0
-	buf.begin_index++
-	if buf.begin_index == buf.length {
-		buf.begin_index = 0
+	b := buf.array[buf.head]
+	buf.array[buf.head] = 0
+	buf.head++
+	if buf.head == buf.length {
+		buf.head = 0
 	}
 	return b, nil
 }
 
 func (buf *Buffer) WriteByte(c byte) error {
-	if buf.array[buf.current_index] != 0 {
+	if buf.IsFull() {
 		return errors.New("The buffer is full!")
 	}
-	buf.array[buf.current_index] = c
-	buf.current_index++
-	if buf.current_index == buf.length {
-		buf.current_index = 0
+	buf.array[buf.tail] = c
+	buf.tail++
+	if buf.tail == buf.length {
+		buf.tail = 0
 	}
 	return nil
 }
 func (buf *Buffer) Overwrite(c byte) {
 	err := buf.WriteByte(c)
 	if err != nil {
-		buf.array[buf.begin_index] = c
-		buf.begin_index++
-		if buf.begin_index == buf.length {
-			buf.begin_index = 0
+		buf.array[buf.head] = c
+		buf.head++
+		if buf.head == buf.length {
+			buf.head = 0
 		}
 	}
 }
 func (buf *Buffer) Reset() {
-	buf.current_index = 0
-	buf.begin_index = 0
+	buf.head, buf.tail = 0, 0
 	buf.array = make([]byte, buf.length)
 }
